82d979308026950a2e647ae6268ebd90351d97e9
Arpit Kaithwar <arpitkaithwar02@gmail.com>
2023-08-14 00:31:33 +0530

    structure updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Blog from './Components/Blog';
+import Team from './Components/Team';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Home/>
-    </>
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/blog" element={<Blog/>}></Route>
+          <Route path="/team" element={<Team/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          
+        </Routes> 
+      </Router>
+       
+
+    <Footer/>
+  </div>
   );
 }
 
---
+++ src/database/teamdata.js
@@ -0,0 +1,14 @@
+var CoreTeam = [
+    {
+    name: "Prem Milind",
+    post: "Manager, Policy Research",
+    fb: "https://www.facebook.com/premmilindgujrathi/",
+    insta: "https://www.instagram.com/premmilindgujrathi/",
+    linkedin: "https://www.linkedin.com/in/premmilindgujrathi/",
+    mail: "mailto:amanrk21@iitk.ac.in",
+    image: "https://i.ibb.co/n6dQT4S/Picture5.jpg"
+  },
+  {
+
+  }
+]
